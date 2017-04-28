4f3cb04d25d3dc979373c298627192d521f81e04
tomo <tomo.mouse.9086@gmail.com>
2017-04-28 12:15:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# git change author
+usage: gitp [\<commit message\>][-f \<ssh address\>] [-h]  
+	-f: first commit  
+	-h: help  
---
+++ gitChangeAuthor.sh
@@ -0,0 +1,53 @@
+gethelp() {
+	echo "usage: gitp [<commit message>][-f <ssh address>] [-h]"
+	echo "	-f: first commit"
+	echo "	-h: help"
+	exit 1
+}
+
+name="******"
+email="*********@example.com"
+
+while getopts f:h OPT
+do
+	case $OPT in
+		f)	status="first_commit"
+			address=$OPTARG
+			echo $address
+			if [ -z $address ]; then
+				exit 1
+			fi
+			;;
+		h)	status="help"
+			gethelp
+			;;
+		\?) status="unexpected_option"
+			gethelp
+            ;;
+	esac
+done
+
+if [ $# -eq 0 ]; then
+	gethelp
+	exit 1
+elif [ -z $status ]; then
+	commit_message=$*
+	# echo "pulling..."
+	git pull
+	git add -A
+	# echo "committing..."
+	git commit -m "${commit_message}"
+	# echo "pushing..."
+	git push
+	exit 0
+fi
+
+if [ -n $address ]; then
+	git config --global user.name $name
+	git config --global user.email $email
+	git init
+	git add -A
+	git commit -m "first commit"
+	git remote add origin $address
+	git push -u origin master
+fi
